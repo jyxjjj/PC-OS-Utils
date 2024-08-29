9912af730aa196fe6dc055c76b5a23862c00e9df
DESMG-ShenLin <773933146@qq.com>
2024-08-29 12:06:10 +0800

    Update and rename build.sh to build.sh

---
+++ ScreenShotsWatcher/build.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+swiftc -O ScreenShotsWatcher.swift -o ScreenShotsWatcher
--- macOS-ScreenShotWatcher/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gcc -fmodules -flto=full -Wall -Wextra -Wpedantic -std=c11 -O3 -o screenshotwatcher screenshotwatcher.c
