00e547418c5775b85387e9d874e8e4888b733173
DESMG-ShenLin <773933146@qq.com>
2025-02-07 04:13:48 +0000

    autostart

--- ScreenShotsWatcher/run.sh
+++ ScreenShotsWatcher/run.sh
@@ -2,9 +2,10 @@
 if [ ! -d ~/Downloads/ScreenShots ]; then
     echo "Folder ~/Downloads/ScreenShots not found, Please create it first."
     echo "And make sure your /System/Applications/Utilities/Screenshot.app's save location is set to save to ~/Downloads/ScreenShots"
+    echo "Maybe you can set defaults of /System/Applications/Utilities/Screenshot.app via settings.sh"
     exit 1
 fi
-cat <<EOF > ~/Downloads/ScreenShots/ScreenShotsWatcher.plist
+cat <<EOF > $HOME/Library/LaunchAgents/ScreenShotsWatcher.plist
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
@@ -23,6 +24,8 @@ cat <<EOF > ~/Downloads/ScreenShots/ScreenShotsWatcher.plist
 </plist>
 EOF
 
-launchctl unload $HOME/Downloads/ScreenShots/ScreenShotsWatcher.plist
-launchctl load $HOME/Downloads/ScreenShots/ScreenShotsWatcher.plist
+launchctl unload $HOME/Library/LaunchAgents/ScreenShotsWatcher.plist 2>/dev/null
+launchctl load $HOME/Library/LaunchAgents/ScreenShotsWatcher.plist 2>/dev/null
+
+ls -al $HOME/Library/LaunchAgents/ScreenShotsWatcher.plist
 ps -ef | grep ScreenShotsWatcher | grep -v grep
